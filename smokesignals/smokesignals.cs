41a4d8d839ad939dee5a15cc23f45c8542214d60
Chris Jones <chris@richmondday.com>
2011-02-23 16:02:03 -0500

    simplified smokesignals.cs a lot

--- smokesignals/smokesignals.cs
+++ smokesignals/smokesignals.cs
@@ -2,80 +2,73 @@
 using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
-namespace smokesignals {
-    public static partial class smokesignals {
+public static class smokesignals {
+    /// <summary>
+    /// Writes a message to the page (in the messageHolder if provided) and decorates the message box with some nice css based on 
+    /// what MessageType you want to show.
+    /// </summary>
+    public static void SendMessage(this Page page, PlaceHolder messageHolder, MessageType messageType, string message, bool append) {
+        Instantiate(page);
+       
+        PlaceHolder plhMessages = messageHolder == null ? page.FindControl("plhMessages") as PlaceHolder : messageHolder;
+        if (plhMessages != null) {
+            // build the div to host the message
+            HtmlGenericControl messageElement = new HtmlGenericControl("div");
+            messageElement.Attributes.Add("class", GetCcss(messageType));
+            messageElement.InnerHtml = string.Format("<p>{0}</p>", message);
 
-        public static void SendMessage(this Page page, MessageType messageType, string message) {
-            SendMessage(page, messageType, message, false);
-        }
+            if (!append) plhMessages.Controls.Clear(); // clear controls if append is false
 
-        public static void SendMessage(this Page page, MessageType messageType, string message, bool append) {
-            Instantiate(page);
-
-            // for now we will assume that the control we'll add our messages to is called plhMessage
-            PlaceHolder plhMessages = page.FindControl("plhMessages") as PlaceHolder;
-
-            if (plhMessages != null) {
-                // build the div to host the message
-                HtmlGenericControl messageElement = new HtmlGenericControl("div");
-                messageElement.Attributes.Add("class", GetCcss(messageType));
-                messageElement.InnerHtml = string.Format("<p>{0}</p>", message);
-
-                if (!append)
-                    plhMessages.Controls.Clear();
-
-                plhMessages.Controls.AddAt(plhMessages.Controls.Count, messageElement);
-            }
-        }
-
-        /// <summary>
-        /// Determines what css class to use based on the MessageType enum
-        /// </summary>
-        /// <param name="messageType">MessageType enum</param>
-        /// <returns>a string containing a class name</returns>
-        private static string GetCcss(MessageType messageType) {
-            string cssClass = string.Empty;
-            switch (messageType) {
-                case MessageType.Error:
-                    cssClass = "Message_Error";
-                    break;
-                case MessageType.Warning:
-                    cssClass = "Message_Warning";
-                    break;
-                case MessageType.Success:
-                    cssClass = "Message_Success";
-                    break;
-                case MessageType.Info:
-                    cssClass = "Message_Info";
-                    break;
-                default:
-                    break;
-            }
-            return cssClass;
-        }
-
-        /// <summary>
-        /// Wriites any embedded resources to the page (css in this case)
-        /// </summary>
-        /// <param name="page"></param>
-        private static void Instantiate(Page page) {
-            // get the embedded css so we can embed it on the page
-            if (!page.ClientScript.IsClientScriptBlockRegistered("messagesCss")) {
-                string css_url = page.ClientScript.GetWebResourceUrl(typeof(smokesignals), "smokesignals.messages.css");
-                string css_out = string.Format("<link href='{0}' type='text/css' rel='stylesheet' />", css_url);
-
-                page.ClientScript.RegisterClientScriptBlock(page.GetType(), "messagesCss", css_out, false);
-            }
+            plhMessages.Controls.AddAt(plhMessages.Controls.Count, messageElement);
         }
     }
     
     /// <summary>
-    /// enum class to keep track of message types.
+    /// Overloaded SendMessage (doesn't require messageHolder or append)
     /// </summary>
-    public enum MessageType {
-        Error,
-        Warning,
-        Success,
-        Info
+    public static void SendMessage(this Page page, MessageType messageType, string message) {
+        SendMessage(page, null, messageType, message, false);
+    }
+    
+    /// <summary>
+    /// Overloaded SendMessage (doesn't require messageHolder)
+    /// </summary>
+    public static void SendMessage(this Page page, MessageType messageType, string message, bool append) {
+        SendMessage(page, null, messageType, message, append);
+    }
+    
+    /// <summary>
+    /// Determines what css class to use based on the MessageType enum
+    /// </summary>
+    private static string GetCcss(MessageType messageType) {
+        switch (messageType) {
+            case MessageType.Error:
+                return "Message_Error";
+            case MessageType.Warning:
+                return "Message_Warning";
+            case MessageType.Success:
+                return "Message_Success";
+            case MessageType.Info:
+                return "Message_Info";
+            default:
+                return "Message_Info";
+                break;
+        }
+    }
+    
+    /// <summary>
+    /// Wriites any embedded resources to the page (css in this case)
+    /// </summary>
+    private static void Instantiate(Page page) {
+        // get the embedded css so we can embed it on the page
+        if (!page.ClientScript.IsClientScriptBlockRegistered("messagesCss")) {
+            string css_out = string.Format("<link href='{0}' type='text/css' rel='stylesheet' />", page.ClientScript.GetWebResourceUrl(typeof(smokesignals), "smokesignals.messages.css"));
+            page.ClientScript.RegisterClientScriptBlock(page.GetType(), "messagesCss", css_out, false);
+        }
     }
 }
+
+/// <summary>
+/// enum class to keep track of and identify message types.
+/// </summary>
+public enum MessageType { Error,Warning,Success,Info }
